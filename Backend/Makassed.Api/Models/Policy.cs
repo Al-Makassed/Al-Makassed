bc66537973db86546869996c6b9097c1b2250ba6
Faris Abuali <fabuali@restaurant365.com>
2023-09-29 12:59:20 +0000

    Apply 1 suggestion(s) to 1 file(s)

--- Backend/Makassed.Api/Models/Policy.cs
+++ Backend/Makassed.Api/Models/Policy.cs
@@ -1,19 +1,18 @@
-﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations;
 
 namespace Makassed.Api.Models
 {
     public class Policy
     {
-        [Key]
-        public string Code { get; set; } = null!;
+        [Key] public string Code { get; set; } = null!;
         public string Name { get; set; } = null!;
-        public bool state { get; set; } = false;
+        public bool State { get; set; } = false;
         public string? PdfUrl { get; set; }
         public Guid ChapterId { get; set; }
 
         // navigation properties
         public Chapter Chapter { get; set; } = null!;
-        ICollection<Dependency> Dependencies { get; } = new List<Dependency>();
 
+        public List<Dependency> Dependencies { get; set; } = new();
     }
 }
