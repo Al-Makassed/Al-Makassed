50bdd0bc2c25ffd9d70159571e7e07054f1911e4
Faris Abuali <fabuali@restaurant365.com>
2023-09-29 12:58:46 +0000

    Apply 1 suggestion(s) to 1 file(s)

--- Backend/Makassed.Api/Models/Dependency.cs
+++ Backend/Makassed.Api/Models/Dependency.cs
@@ -1,20 +1,25 @@
-﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations;
+using Makassed.Api.Enums;
 
-namespace Makassed.Api.Models
+namespace Makassed.Api.Models;
+
+public class Dependency
 {
-    public class Dependency
-    {
-        [Key]
-        public string Code { get; set; } = null!;
-        public string? PdfUrl { get; set; }
-        public int EstimatedTime { get; set; }
-        public int PagesCount { get; set; }
-        public string PolicyCode { get; set; } = null!;
-        public Guid DependencyTypeId { get; set; }
+    [Key]
+    public string Code { get; set; } = null!;
 
-        // navigation properties
-        public Policy Policy { get; set; } = null!;
-        public DependencyType DependencyType { get; set; } = null!;
+    public string? PdfUrl { get; set; }
 
-    }
+    public int EstimatedTime { get; set; }
+
+    public int PagesCount { get; set; }
+
+    public string PolicyCode { get; set; } = null!;
+
+    public Guid DependencyTypeId { get; set; }
+
+    public DependencyType DependencyType { get; set; }
+
+    // navigation properties
+    public Policy Policy { get; set; } = null!;
 }
