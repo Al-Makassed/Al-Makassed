44c111ff5ea5bf655e111b7c76861f0be01420cf
Faris Abuali <faris.abuali@outlook.com>
2023-09-30 22:36:24 +0300

    Refactor Login & setup MUI ThemeProvider & MaqasidTheme

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -1,21 +1,20 @@
-import React from "react";
+import {FC} from "react";
 import "./App.css";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Login from "./Pages/Components/Login";
+import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
+import Login from "./Pages/Login";
 
+const App: FC = () => {
+    return (
+        <div className="App">
+            <Router>
+                <Routes>
+                    <Route path="/" element={<Login/>}/>
+                    <Route path="/login" element={<Login/>}/>
 
-const App: React.FC = () => {
-  return (
-    <div className="App">
-      <Router>
-        <Routes>
-          <Route path="/" element={<Login />} />
-          <Route path="/login" element={<Login />} />
-
-        </Routes>
-      </Router>
-    </div>
-  );
+                </Routes>
+            </Router>
+        </div>
+    );
 };
 
 export default App;
---
+++ Frontend/src/Pages/Home/Home.tsx
@@ -0,0 +1,12 @@
+import { FC } from 'react';
+
+interface HomeProps {}
+
+const Home: FC<HomeProps> = () => {
+
+  return (
+    <div>Home</div>
+  );
+};
+
+export default Home;
