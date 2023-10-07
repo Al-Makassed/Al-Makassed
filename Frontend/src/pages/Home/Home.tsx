5832548c207134173650a834e40dd49f169d0ea7
Faris Abuali <faris.abuali@outlook.com>
2023-10-07 06:39:08 +0300

    Test netlify deploy

--- Frontend/src/pages/Home/Home.tsx
+++ Frontend/src/pages/Home/Home.tsx
@@ -2,7 +2,7 @@ import React, { FC } from "react";
 import Typography from "@mui/material/Typography";
 
 const Home: FC = () => {
-  return <Typography variant="h3">Welcome to Al Maqasid</Typography>;
+  return <Typography variant="h3">Welcome to Al-Maqasid</Typography>;
 };
 
 export default Home;
