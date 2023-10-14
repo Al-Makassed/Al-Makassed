4ad87d3ad94ca582fd725d81f2d9fda288035f55
Faris Abuali <faris.abuali@outlook.com>
2023-10-15 00:55:00 +0300

    MAK-18 Rename Pages folder to pages

--- Frontend/src/pages-new/Home/Home.tsx
+++
@@ -1,8 +0,0 @@
-import React, { FC } from "react";
-import Typography from "@mui/material/Typography";
-
-const Home: FC = () => {
-  return <Typography variant="h3">Welcome to Al-Maqasid</Typography>;
-};
-
-export default Home;
