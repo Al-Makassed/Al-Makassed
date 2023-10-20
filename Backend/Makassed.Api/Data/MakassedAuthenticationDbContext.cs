d49a4e67168548a6b21224e239e5db9a9e2b838d
HayaAbuRaed <hayamousa78@gmail.com>
2023-10-20 17:59:42 +0300

    MAK-26/ Create the authintication DB context and seeded data to Roles.

--- Backend/Makassed.Api/Data/MakassedAuthenticationDbContext.cs
+++ Backend/Makassed.Api/Data/MakassedAuthenticationDbContext.cs
@@ -1,6 +1,8 @@
+using Makassed.Api.Data.Configuration;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using System.Reflection;
 
 namespace Makassed.Api.Data;
 
@@ -13,7 +15,18 @@ public class MakassedAuthenticationDbContext : IdentityDbContext<IdentityUser>
     protected override void OnModelCreating(ModelBuilder builder)
     {
         base.OnModelCreating(builder);
-        
-        builder.HasDefaultSchema("Identity");
+
+        // Customize the Identity tables names
+        builder.Entity<IdentityUser>().ToTable("Users");
+        builder.Entity<IdentityRole>().ToTable("Roles");
+        builder.Entity<IdentityUserClaim<string>>().ToTable("UserClaims");
+        builder.Entity<IdentityUserRole<string>>().ToTable("UserRoles");
+        builder.Entity<IdentityUserLogin<string>>().ToTable("UserLogins");
+        builder.Entity<IdentityUserToken<string>>().ToTable("UserTokens");
+        builder.Entity<IdentityRoleClaim<string>>().ToTable("RoleClaims");
+
+        builder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+
+        //builder.ApplyConfiguration(new IdentityRoleConfiguration());
     }
-}
+} 
