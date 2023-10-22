0c23c2febc1aeb7c07d1bbc513cb9534704cb1e6
Faris Abuali <faris.abuali@outlook.com>
2023-10-23 00:44:14 +0300

    Make options argument type for the MakassedDbContext as DbContextOptions<MakassedDbContext>

--- Backend/Makassed.Api/Data/MakassedDbContext.cs
+++ Backend/Makassed.Api/Data/MakassedDbContext.cs
@@ -2,21 +2,20 @@ using Makassed.Api.Models;
 using Makassed.Api.Models.Domain;
 using Microsoft.EntityFrameworkCore;
 
-namespace Makassed.Api.Data
+namespace Makassed.Api.Data;
+
+public class MakassedDbContext : DbContext
 {
-    public class MakassedDbContext : DbContext
+    public MakassedDbContext(DbContextOptions<MakassedDbContext> options) : base(options)
     {
-        public MakassedDbContext(DbContextOptions options) : base(options)
-        {
-        }
-
-        public DbSet<Chapter> Chapters { get; set; } = null!;
-        public DbSet<Policy> Policies { get; set; } = null!;
-        public DbSet<Dependency> Dependencies { get; set; } = null!;
-
-        // protected override void OnModelCreating(ModelBuilder modelBuilder)
-        // {
-        //     base.OnModelCreating(modelBuilder);
-        // }
     }
+
+    public DbSet<Chapter> Chapters { get; set; } = null!;
+    public DbSet<Policy> Policies { get; set; } = null!;
+    public DbSet<Dependency> Dependencies { get; set; } = null!;
+
+    // protected override void OnModelCreating(ModelBuilder modelBuilder)
+    // {
+    //     base.OnModelCreating(modelBuilder);
+    // }
 }
