e6f0c7bd54500f61c384afe5895d195e29f4e968
Faris Abuali <faris.abuali@outlook.com>
2023-10-23 03:57:04 +0300

    MAK-39 Introduce Cleaner Folder Structure for Dependency Injection

---
+++ Backend/Makassed.Api/Dependencies/FluentValidationConfiguration.cs
@@ -0,0 +1,36 @@
+﻿namespace Makassed.Api.Dependencies;
+
+using Microsoft.Extensions.DependencyInjection;
+using FluentValidation;
+using FluentValidation.AspNetCore;
+using System.Reflection;
+
+public static class FluentValidationConfiguration
+{
+    public static IServiceCollection AddMakassedFluentValidation(IServiceCollection services)
+    {
+        // Add the `FluentValidation package` services to the DI container
+        services
+            .AddFluentValidationAutoValidation()
+            .AddFluentValidationClientsideAdapters();
+
+        // Get all types in the assembly that implement AbstractValidator
+        var validatorTypes = Assembly.GetCallingAssembly().GetTypes()
+            .Where(t => t.BaseType != null && t.BaseType.IsGenericType &&
+                        t.BaseType.GetGenericTypeDefinition() == typeof(AbstractValidator<>));
+
+        // Register each validator
+        foreach (var validatorType in validatorTypes)
+        {
+            // Get the generic argument of AbstractValidator<>
+            var targetType = validatorType.BaseType?.GetGenericArguments().FirstOrDefault();
+            if (targetType != null)
+            {
+                // Register the validator with the specific type it validates
+                services.AddScoped(typeof(IValidator<>).MakeGenericType(targetType), validatorType);
+            }
+        }
+
+        return services;
+    }
+}
---
+++ Backend/Makassed.Api/DependencyInjection.cs
@@ -0,0 +1,28 @@
+﻿using Makassed.Api.Dependencies;
+using Makassed.Api.Mappings;
+
+namespace Makassed.Api;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection RegisterMakassedDependencies(this IServiceCollection services, IConfiguration configuration)
+    {
+        CorsConfiguration.AddMakassedCors(services, configuration);
+
+        FluentValidationConfiguration.AddMakassedFluentValidation(services);
+
+        ApplicationServicesConfiguration.RegisterMakassedServices(services);
+
+        AuthenticationServiceConfiguration.AddJwtAuthentication(services, configuration);
+        
+        IdentityConfiguration.AddIdentity(services);
+        
+        DbContextsConfiguration.AddDbContexts(services, configuration);
+        
+        MailKitEmailConfiguration.AddEmailService(services, configuration);
+
+        services.AddAutoMapper(typeof(AutoMapperProfile));
+
+        return services;
+    }
+}
