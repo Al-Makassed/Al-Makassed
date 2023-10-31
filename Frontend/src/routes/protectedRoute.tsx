ec6d999dd065d18a08b15b9be28cbebcd3504ea3
israa-yahya <israayahya23@gmail.com>
2023-10-31 16:00:08 +0200

    MAK-25 Test Piplined

--- Frontend/src/routes/protectedRoute.tsx
+++ Frontend/src/routes/protectedRoute.tsx
@@ -1,14 +1,14 @@
-import React from "react";
-import { Outlet, useNavigate } from "react-router-dom";
+// import React from "react";
+// import { Outlet, useNavigate } from "react-router-dom";
 
-const protectedRoute = () => {
-  const navigate = useNavigate();
+// const protectedRoute = () => {
+//   const navigate = useNavigate();
 
-  if (!localStorage.getItem("accessToken")) {
-    return navigate("/login");
-  } else {
-    return <Outlet />;
-  }
-};
+//   if (!localStorage.getItem("accessToken")) {
+//     return navigate("/login");
+//   } else {
+//     return <Outlet />;
+//   }
+// };
 
-export default protectedRoute;
+// export default protectedRoute;
