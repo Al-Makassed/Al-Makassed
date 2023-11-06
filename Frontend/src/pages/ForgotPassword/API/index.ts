ecc25ef38f0fae83b56ded4b5b03fdf6bd0024ea
israa-yahya <israayahya23@gmail.com>
2023-11-06 17:25:25 +0200

    MAK-56 Resolve for all commit to MAK-56

--- Frontend/src/pages/ForgotPassword/API/index.ts
+++ Frontend/src/pages/ForgotPassword/API/index.ts
@@ -1,5 +1,5 @@
 import axios from "src/API/axios";
-import { BaseSuccessResponse } from "src/API/axios";
+import { BaseSuccessResponse } from "src/types";
 
 export const forgotPassword = (userID: string) => {
   return axios
