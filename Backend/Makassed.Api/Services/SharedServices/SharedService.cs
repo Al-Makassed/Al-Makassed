2dd6371630d72013993108806b21a83c836ecef2
Haya Abu-Raed <hayaa.aburaed@gmail.com>
2023-11-17 07:18:35 +0200

    MAK-78/ Clean other services due to the previous changes.

--- Backend/Makassed.Api/Services/SharedServices/SharedService.cs
+++ Backend/Makassed.Api/Services/SharedServices/SharedService.cs
@@ -30,22 +30,4 @@ public class SharedService : ISharedService
         using PdfDocument pdfDoc = new(new PdfReader(file.OpenReadStream()));
         return pdfDoc.GetNumberOfPages();
     }
-
-    public string GetCode(string parentName, string name, int siblingsCount)
-    {
-        var parentAbbreviation = new string(parentName.Split(' ').Select(s => s[0]).ToArray());
-
-        var instanceNameAbbreviation = new string(name.Split(' ').Select(s => s[0]).ToArray());
-
-        return $"{parentAbbreviation.ToUpper()}. {instanceNameAbbreviation.ToUpper()} -{siblingsCount + 1}";
-    }
-
-    public string UpdateCode(string oldCode, string newName, int index)
-    {
-        var abbreviationToChange = new string(newName.Split(' ').Select(s => s[0]).ToArray());
-        
-        var codeParts = oldCode.Split(' ');
-
-        return index == 0 ? $"{abbreviationToChange.ToUpper()}. {codeParts[1]} {codeParts[2]}" : $"{codeParts[0]} {abbreviationToChange.ToUpper()} {codeParts[2]}";
-    }
 }
