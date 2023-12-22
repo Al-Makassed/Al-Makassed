5c366368a4af1059d66e27a8d5137f996c54c661
Haya Abu-Raed <hayaa.aburaed@gmail.com>
2023-12-22 21:12:29 +0200

    MAK-99/ Modified Add Policy Dialog: - Make the summary not required. - Make the summary field multiline. - Removed unnecessary useEffect.

--- Frontend/src/pages/AddPolicyDialog/schema.ts
+++ Frontend/src/pages/AddPolicyDialog/schema.ts
@@ -4,10 +4,6 @@ import { AddPolicyFormPayload } from "./types";
 const validationSchema = Yup.object<AddPolicyFormPayload>().shape({
   Name: Yup.string().nullable().required("Policy name is required"),
   Code: Yup.string().nullable().required("Policy code is required"),
-  Summary: Yup.string()
-    .nullable()
-    .required("Policy summary is required")
-    .max(1000, "Too long summary"),
   EstimatedTimeInMin: Yup.number()
     .max(60, "Too long estimated time")
     .positive("must be a positive number")
