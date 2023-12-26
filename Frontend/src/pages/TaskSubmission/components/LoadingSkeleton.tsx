052fd963ad8abfb1b47b13110ce67273f3141696
Haya Abu-Raed <hayaa.aburaed@gmail.com>
2023-12-26 08:56:23 +0200

    MAK-100/ Loading skeleton style fixes.

--- Frontend/src/pages/TaskSubmission/components/LoadingSkeleton.tsx
+++ Frontend/src/pages/TaskSubmission/components/LoadingSkeleton.tsx
@@ -4,24 +4,17 @@ import FieldsListSkeleton from "./FieldsListSkeleton";
 
 const LoadingSkeleton: FC = () => {
   return (
-    <Grid container p={4}>
+    <Grid container px={3}>
+      <Typography variant="h2" width={"50%"} mb={2}>
+        <Skeleton />
+      </Typography>
+
       <Grid item width={"100%"} xs={12} md={8}>
-        <Typography variant="h1" width={"50%"}>
-          <Skeleton />
-        </Typography>
-
-        <Typography variant="h6" width={"20%"} sx={{ mt: "auto" }}>
-          <Skeleton />
-        </Typography>
-
         <FieldsListSkeleton />
       </Grid>
 
-      <Grid item md={4} pl={3} pt={3}>
-        <Skeleton
-          variant="rounded"
-          height={"calc(100vh - 64px - 32px - 32px)"}
-        />
+      <Grid item sm={0} md={4} pl={3}>
+        <Skeleton variant="rounded" height={"calc(100vh - 64px - 113px)"} />
       </Grid>
     </Grid>
   );
