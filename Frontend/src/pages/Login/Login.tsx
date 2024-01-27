66d0d0cbc6f6288190d9cc90e3f591aa3cc4fff3
Faris Abuali <fabuali@restaurant365.com>
2024-01-28 01:29:15 +0200

    Remove Login Background for mobile screens

--- Frontend/src/pages/Login/Login.tsx
+++ Frontend/src/pages/Login/Login.tsx
@@ -2,11 +2,14 @@ import React, { FC } from "react";
 import Grid from "@mui/material/Grid";
 import LoginBackground from "./components/LoginBackground";
 import LoginForm from "./components/LoginForm";
+import useMediaQuery from "src/hooks/useMediaQuery";
 
 const LoginPage: FC = () => {
+  const { isMobile } = useMediaQuery();
+
   return (
     <Grid container sx={{ height: "100vh" }}>
-      <LoginBackground />
+      {!isMobile && <LoginBackground />}
       <LoginForm />
     </Grid>
   );
