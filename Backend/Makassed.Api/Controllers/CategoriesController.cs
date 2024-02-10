3343f72ffe89c2ba761c405c3ef4ee788bb7de17
Haya Abu-Raed <hayaa.aburaed@gmail.com>
2024-02-10 20:11:19 +0200

    MAK-118/ Added Authorization to Categories Controller.

--- Backend/Makassed.Api/Controllers/CategoriesController.cs
+++ Backend/Makassed.Api/Controllers/CategoriesController.cs
@@ -18,7 +18,7 @@ public class CategoriesController : ApiController
     }
 
     [HttpGet]
-    //[Authorize]
+    [Authorize]
     [ProducesResponseType(typeof(List<GetCategoryResponse>), StatusCodes.Status200OK)]
     [ProducesResponseType(StatusCodes.Status401Unauthorized)]
     [ProducesResponseType(StatusCodes.Status500InternalServerError)]
@@ -28,7 +28,7 @@ public class CategoriesController : ApiController
     }
 
     [HttpGet("{id:guid}")]
-    //[Authorize]
+    [Authorize]
     [ProducesResponseType(typeof(GetCategoryResponse), StatusCodes.Status200OK)]
     [ProducesResponseType(StatusCodes.Status401Unauthorized)]
     [ProducesResponseType(StatusCodes.Status404NotFound)]
@@ -45,7 +45,7 @@ public class CategoriesController : ApiController
     }
 
     [HttpPost]
-    //[Authorize(Roles = "Admin, Sub-Admin")]
+    [Authorize(Roles = "Admin, Sub-Admin")]
     [ProducesResponseType(typeof(GetCategoryResponse), StatusCodes.Status201Created)]
     [ProducesResponseType(StatusCodes.Status400BadRequest)]
     [ProducesResponseType(StatusCodes.Status401Unauthorized)]
@@ -61,7 +61,7 @@ public class CategoriesController : ApiController
     }
 
     [HttpPut("{id:guid}")]
-    //[Authorize(Roles = "Admin")]
+    [Authorize(Roles = "Admin")]
     [ProducesResponseType(typeof(GetCategoryResponse), StatusCodes.Status200OK)]
     [ProducesResponseType(StatusCodes.Status400BadRequest)]
     [ProducesResponseType(StatusCodes.Status401Unauthorized)]
@@ -78,7 +78,7 @@ public class CategoriesController : ApiController
     }
 
     [HttpDelete("{id:guid}")]
-    //[Authorize(Roles = "Admin")]
+    [Authorize(Roles = "Admin")]
     [ProducesResponseType(typeof(GetCategoryResponse), StatusCodes.Status200OK)]
     [ProducesResponseType(StatusCodes.Status401Unauthorized)]
     [ProducesResponseType(StatusCodes.Status404NotFound)]
