4e9eb97e93e5a4443daefdcc0dd94b10c0c2f6fe
Faris Abuali <faris.abuali@outlook.com>
2024-03-15 01:33:59 +0200

    Update staging API URL

--- Frontend/src/constants/environment.ts
+++ Frontend/src/constants/environment.ts
@@ -4,6 +4,6 @@ export const ENV_TO_WEB_APP_MAP = {
 };
 
 export const ENV_TO_API_URL_MAP = {
-  staging: "https://maqasid.azurewebsites.net/api",
+  staging: "https://almakassed-dev.azurewebsites.net/api",
   production: "https://almakassed.azurewebsites.net/api",
 };
